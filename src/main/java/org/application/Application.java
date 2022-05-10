e0dffc00415f06e632f71ab4c5cf23638d35876b
Vince Pongracz <pongrvin@gmail.com>
2022-05-10 17:48:50 +0200

    : removed

--- src/main/java/org/application/Application.java
+++ src/main/java/org/application/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-        //Bug here, won't compile:
+        //Bug here, won't compile
         System.out.println("Welcome in gradle")
 
         var ex = new ExampleClass(0, "0 Measurement", Calendar.getInstance().getTime());
